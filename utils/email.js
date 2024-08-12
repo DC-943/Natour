650eb57e3c319bae84a5da8140922d929648ad92
michael_chen943 <michael_chen943@aliyun.com>
2024-08-12 22:37:41 +0800

    add reset password

--- utils/email.js
+++ utils/email.js
@@ -1,9 +1,10 @@
 const nodemailer = require("nodemailer");
 
-const sendEmail = options => {
+const sendEmail = async options => {
   //1) create a transporter
   const transporter = nodemailer.createTransport({
-    service: "Gmail",
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD
@@ -12,5 +13,15 @@ const sendEmail = options => {
   });
 
   //2) define the mail options
+  const mailOptions = {
+    from: "Dong Chen <hello@jonas.io>",
+    to: options.email,
+    subject: options.subject,
+    text: options.message
+  };
   //3) actually send the email
+
+  await transporter.sendMail(mailOptions);
 };
+
+module.exports = sendEmail;
